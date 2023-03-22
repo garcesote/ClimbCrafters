b07020006bfa4a77592a309a2544c4ca36167663
David Amatriain Gurpegui <amatriain.126740@e.unavarra.es>
2023-03-22 20:52:26 +0100

    Añadir página de confirmación de pedido, es un componente reutilizable

--- src/App.js
+++ src/App.js
@@ -13,12 +13,19 @@ import Carrito from './Components/Carrito';
 import Login from './Pages/Login';
 import Signup from './Pages/Signup';
 import DetailProduct from './Pages/DetailProduct';
+import Confirmation from './Pages/Confirmation';
 
 function App() {
 
   const [carrito, setCarrito] = useState(false);
   const [login, setLogin] = useState();
-  const [carritoData, setCarritoData] = useState([]);
+  const [carritoData, setCarritoData] = useState(
+    [
+      {id: "1", nombre: "Roca gorda", descripcion: "Descripción del productaco", precio: 12, cantidad: 3, img: "https://firebasestorage.googleapis.com/v0/b/clase-react-8ce4d.appspot.com/o/carousel-2.jpeg?alt=media&token=8e80bfe5-a437-49b4-bf12-c36f9d3231a2"},
+      {id: "2", nombre: "Roquetes", descripcion: "Tocho tocho tochoooooo", precio: 10, cantidad: 2, img: "https://firebasestorage.googleapis.com/v0/b/clase-react-8ce4d.appspot.com/o/carousel-2.jpeg?alt=media&token=8e80bfe5-a437-49b4-bf12-c36f9d3231a2"},
+      {id: "3", nombre: "Cositas", descripcion: "A mamarla a otro sitio", precio: 5, cantidad: 1, img: "https://firebasestorage.googleapis.com/v0/b/clase-react-8ce4d.appspot.com/o/carousel-2.jpeg?alt=media&token=8e80bfe5-a437-49b4-bf12-c36f9d3231a2"},
+      {id: "4", nombre: "Muchas rocas", descripcion: "Garcesote guele a pito", precio: 100, cantidad: 1, img: "https://firebasestorage.googleapis.com/v0/b/clase-react-8ce4d.appspot.com/o/carousel-2.jpeg?alt=media&token=8e80bfe5-a437-49b4-bf12-c36f9d3231a2"}
+    ]);
   const [loginData, setLoginData] = useState();
   const loginContext = useContext(LoginContext);
   const [email, setEmail] = useState("");
@@ -83,6 +90,7 @@ function App() {
                       <Route path='/login' element={<Login />} />
                       <Route path='/signup' element={<Signup />} />
                       <Route path='/detail-product/:id' element={<DetailProduct />} />
+                      <Route path='/confirmation' element={<Confirmation />} />
                     </Routes>
                   </Col>
                   <Col sm={3} style={{ height: '90vh', backgroundColor: 'lightgrey' }}>
@@ -99,6 +107,7 @@ function App() {
                       <Route path='/login' element={<Login />} />
                       <Route path='/signup' element={<Signup />} />
                       <Route path='/detail-product/:id' element={<DetailProduct />} />
+                      <Route path='/confirmation' element={<Confirmation />} />
                     </Routes>
                   </Col>
                 </Row>
---
+++ src/Components/Order.js
@@ -0,0 +1,54 @@
+import React, { useState, useContext, useEffect } from "react";
+import styles from './Order.css'
+import LoginContext from "../Storage/LoginContext";
+import AutContext from "../Storage/AutContext";
+import { Button, Form, Card, Row, Col, Container, Image } from 'react-bootstrap';
+
+const Order = ({ data }) => {
+
+    const autContext = useContext(AutContext);
+    const loginContext = useContext(LoginContext);
+    const [orderPrice, setOrderPrice] = useState(0);
+    console.log(data)
+
+    useEffect(() => {
+        let price = 0;
+        data.map((product) => {
+            price += product.cantidad * product.precio
+        })
+        setOrderPrice(price)
+    }, [])
+    
+
+    return (
+        <>
+            {
+                data.map((product) => {
+                    return (
+                        <div className="product">
+                            <div className="productName">
+                                <Image
+                                    className="d-block w-80"
+                                    style={{ maxHeight: '50px' }}
+                                    src="https://firebasestorage.googleapis.com/v0/b/clase-react-8ce4d.appspot.com/o/carousel-2.jpeg?alt=media&token=8e80bfe5-a437-49b4-bf12-c36f9d3231a2"
+                                    alt="First slide"
+                                />
+                                <div>{product.nombre}  </div>
+                            </div>
+                            <div className="productPrice">
+                                <div className="quantity"> {product.cantidad}</div>
+                                <div className="price"> {product.precio}$ </div>
+                            </div>
+
+                        </div>
+                    )
+                })
+            }
+            <div className="productDetails">
+                <h3>Order price: {orderPrice} $</h3>
+            </div>
+        </>
+    )
+}
+
+export default Order;
